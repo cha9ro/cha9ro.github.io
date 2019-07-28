e29e2eb2360eb75a4d1439fc644d7562398e8f46
Takuro Takezawa <t910inicident@gmail.com>
2019-07-28 16:54:13 +0900

    temp

--- shellfile/run_jekyll.sh
+++ shellfile/run_jekyll.sh
@@ -1,2 +1,3 @@
 export JEKYLL_VERSION=3.8
-docker run --rm -p 4000:4000 --volume="$PWD:/srv/jekyll" -it --name github-pages jekyll/jekyll:$JEKYLL_VERSION bash
+# docker run --rm -p 4000:4000 --volume="$PWD:/srv/jekyll" -it --name github-pages jekyll/jekyll:$JEKYLL_VERSION bash
+docker run -p 4000:4000 --volume="$PWD:/srv/jekyll" -it --name github-pages jekyll/jekyll:$JEKYLL_VERSION bash
